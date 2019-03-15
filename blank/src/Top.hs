bf312fb21db994e7b9342510fb9c1f53fc9d4e02
Matthias Heinzel <mheinzel@posteo.de>
2019-03-15 02:13:27 +0000

    add minimal src/.hs file

---
+++ blank/src/Top.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE BinaryLiterals             #-}
+{-# LANGUAGE RankNTypes #-}
+module Main where
+
+import Clash.Prelude
+import Control.Lens as Lens (Lens, Lens', over)
+
+{-# ANN topEntity
+  (Synthesize
+    { t_name     = "blinker"
+    , t_inputs   =
+        [ PortName "clk"
+        , PortName "pmod1"
+        , PortName "pmod2"
+        , PortName "pmod3"
+        , PortName "pmod4"
+        ]
+    , t_output   = PortProduct "out"
+        [ PortName "led1"
+        , PortName "led2"
+        , PortName "led3"
+        , PortName "led4"
+        , PortName "led5"
+        ]
+    }) #-}
+topEntity
+  :: Clock System Source
+  -> Bit -> Bit -> Bit
+  -> Signal System (Bit, Bit, Bit, Bit, Bit)
+topEntity clk _ _ _ =
+  pure (high, high, high, high, low)
+
+main :: IO ()
+main = print "hello world"
